87a61c2e3280543f5f3198b82ca9ca3c89dbd29f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-16 20:29:42 +0530

    updated todo

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,1 +1,14 @@
-const { Todo } = require('../server/db/index')
+const express = require('express');
+const {authenticateJwt, SECRET} = require('../middleware/auth');
+const { Todo } = require('../db/index');
+
+const router = express.Router();
+
+router.post('/todos', authenticateJwt, async (req, res) => {
+    const { title, description } = req.body;
+    const done = false;
+    const userId = req.userId;
+
+    const newTodo = new Todo({ title, description, done, userId });
+    
+})
