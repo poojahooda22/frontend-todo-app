94ab708e474f46eafa91a1c9b5e98245c7368f99
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-16 22:31:59 +0530

    renamed the signup

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const signup = () => {
+const Signup = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const signup = () => {
   )
 }
 
-export default signup
+export default Signup
