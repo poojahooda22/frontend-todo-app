a3e3409d15b2673f9cc82cc6a031629c0f9d64ef
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-16 22:59:00 +0530

    updated route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/landingPage';
+import Signup from './components/signup';
 
 
 
@@ -10,7 +11,8 @@ function App() {
   return (
     <Router>
       <div className='w-full h-screen bg-zinc-900 text-white'>
-      <LandingPage/>
+      <Route path='/' element={<LandingPage />}/>
+      <Route path='/signup' element={<Signup/>} />
 
     </div>
     </Router>
