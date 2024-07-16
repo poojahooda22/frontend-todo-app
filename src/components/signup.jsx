721273aa922f32ae43b2074134db3200ddaaefc8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-16 23:09:57 +0530

    updated signup page

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -2,8 +2,12 @@ import { Card, TextField } from "@mui/material"
 
 const Signup = () => {
   return (
-    <div className="">
-        <Card>
+    <div className="flex items-center justify-center mt-[2vw]">
+        <Card style={{
+                    padding: '16px',   width: 400, 
+                    display:"flex", flexDirection: "column", 
+                    justifyContent: "center",  alignItems: "center" 
+                }}>
             <TextField id="outlined-basic" label="Outlined" variant="outlined" />
         </Card>
     </div>
