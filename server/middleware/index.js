48a38246a9fbe2d88ba8448475c03bf5fda9d52f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-16 21:38:13 +0530

    created index

---
+++ server/middleware/index.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const { Response } = require('express');
+const SECRET = "my-secret-key";
+
+
+const authenticateJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, SECRET, (err, user) => {
+            if(err) {
+                return res.sendStatus(403);
+            }
+            req.userId = user.id;
+            next();
+        })
+    }
+    else {
+        res.sendStatus(401);
+    }
+};
+
+module.exports = {
+    authenticateJwt,
+    SECRET
+}
