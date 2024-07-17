96ac2faaa553f9849363f95cf8a5511e388132b9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-17 14:58:10 +0530

    updated userstate

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react"
 import Appbar from "./Appbar"
 
 
 const LandingPage = () => {
+
+  const [username, setUsername] = useState('');
+
   return (
     <div>
       <Appbar />
-      
     </div>
   )
 }
