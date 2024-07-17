6224d3d3075236138be949686f2af0fe9f447001
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-17 15:55:34 +0530

    removed navigate

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"
 
 
 const Appbar = () => {
-    const navigate = useNavigate();
+    
   return (
     <div className="flex items-center justify-center px-[2vw] py-[1.2vw]">
         {/* <div><h2 className="text-[1vw] font-semibold text-center">Todo App</h2></div> */}
