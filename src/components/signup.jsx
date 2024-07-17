ef31de723500cfae69a18426cdbc5a6c8db2db22
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-17 11:04:18 +0530

    updated signup

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,16 +1,23 @@
-import { Card, TextField } from "@mui/material"
+import { Button, Card, TextField } from "@mui/material"
 
 const Signup = () => {
   return (
-    <div className="flex items-center justify-center pt-[2vw]">
+    <div className="flex items-center justify-center pt-[14vw]">
         <Card style={{
             padding: '16px',   width: 400, 
             display:"flex", flexDirection: "column", 
             justifyContent: "center",  alignItems: "center" }}
         >
-            <TextField label="Email" variant="outlined" size="small" margin="normal" />
-            <TextField label="password" variant="outlined" size="small" margin="normal" />
-            
+            <h2 className="text-[1vw] font-bold">Welcome to Todo App!</h2>
+            <TextField style={{width: '360px', height: '50px'}}  label="Email" variant="outlined" size="small" margin="normal" />
+            <TextField style={{width: '360px', height: '50px'}}  label="password" variant="outlined" size="small" margin="normal" />
+            <Button variant="contained" 
+                margin="normal"
+                size="large"
+                style={{
+                    marginTop: '16px',        
+                }}
+            >Signup</Button>
         </Card>
     </div>
   )
