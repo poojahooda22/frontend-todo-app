c953d098692bec4a554b6559d045e1fe2fb8b88a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-17 12:16:02 +0530

    updated appbar

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -6,15 +6,17 @@ import { useNavigate } from "react-router-dom"
 const Appbar = () => {
     const navigate = useNavigate();
   return (
-    <div className="flex items-center justify-between px-[2vw] py-[1.2vw]">
+    <div className="flex items-center justify-center px-[2vw] py-[1.2vw]">
         <div><h2 className="text-[1vw] font-semibold">Todo App</h2></div>
-        <div>
+        {/* <div>
             <Button variant="contained" margin="normal" size="large"
                 onClick={() => {
                     navigate('/signup')
                 }}
             >Signup</Button>
-        </div>
+        </div> */}
+
+        
     </div>
   )
 }
