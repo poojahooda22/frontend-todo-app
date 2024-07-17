ba3d3dcbaeec025736ba3c07d324510e588e9f15
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-17 12:51:38 +0530

    updated link tag

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,4 +1,5 @@
 import { Button, Card, TextField } from "@mui/material"
+import { Link } from "react-router-dom"
 
 const Signup = () => {
   return (
@@ -6,23 +7,23 @@ const Signup = () => {
         <Card style={{
             padding: '16px',   width: 460, 
             display:"flex", flexDirection: "column", 
-            justifyContent: "center",  alignItems: "center" }}
+            justifyContent: "center",  alignItems: "center",  }}
         >   
-            <h2 className="text-[1.3vw] font-bold text-center mb-[.2vw]">Hello</h2>
+            
             <h2 className="text-[.9vw] font-medium text-center mb-[1vw]">Welcome! Please enter your details</h2>
             <TextField style={{width: '360px', height: '50px'}}  label="Email" variant="outlined" size="small" margin="normal" />
             <TextField style={{width: '360px', height: '50px'}}  label="password" variant="outlined" size="small" margin="normal" />
             <div className="flex items-center gap-4 py-[1vw]">
-                <div className="w-[6vw] h-[.1px] border-[.1px] border-zinc-600"></div>
+                <div className="w-[6vw] h-[.1px] border-[.1px] border-zinc-300"></div>
                 <p>OR</p>
-                <div className="w-[6vw] h-[.1px] border-[.1px] border-zinc-600"></div>
+                <div className="w-[6vw] h-[.1px] border-[.1px] border-zinc-300"></div>
             </div>
-            <p>Already have an account <span className="underline text-blue-500 font-semibold">Login</span></p>
+            <Link to="/login" className="text-[.7vw]">Already have an account <span className="underline text-blue-500 font-semibold">Login</span></Link>
             <Button variant="contained" 
                 margin="normal"
                 size="large"
                 style={{
-                    marginTop: '2vw',        
+                    marginTop: '1vw',        
                 }}
             >Signup</Button>
         </Card>
