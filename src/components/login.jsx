73046b17a08b9a395259077bea9cb6f4ab569a48
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-17 12:57:56 +0530

    updated format

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -4,29 +4,28 @@ import { Link } from "react-router-dom"
 const Login = () => {
   return (
     <div className="flex items-center justify-center pt-[14vw]">
-        <Card style={{
-            padding: '16px',   width: 460, 
-            display:"flex", flexDirection: "column", 
-            justifyContent: "center",  alignItems: "center",  }}
-        >   
-            
-            <h2 className="text-[.9vw] font-medium text-center mb-[1vw]">Welcome again! </h2>
-            <TextField style={{width: '360px', height: '50px'}}  label="Email" variant="outlined" size="small" margin="normal" />
-            <TextField style={{width: '360px', height: '50px'}}  label="password" variant="outlined" size="small" margin="normal" />
-            <Button variant="contained" 
-                margin="normal"
-                size="large"
-                style={{
-                    marginTop: '1vw',        
-                }}
-            >Login</Button>
-            <div className="flex items-center gap-4 py-[1vw]">
-                <div className="w-[6vw] h-[.1px] border-[.1px] border-zinc-300"></div>
-                <p>OR</p>
-                <div className="w-[6vw] h-[.1px] border-[.1px] border-zinc-300"></div>
-            </div>
-            <Link to="/signup" className="text-[.7vw]">Don&apos;t have an account <span className="underline text-blue-500 font-semibold">Signup</span></Link> 
-        </Card>
+      <Card style={{
+        padding: '16px',   width: 460, 
+        display:"flex", flexDirection: "column", 
+        justifyContent: "center",  alignItems: "center",  }}
+      >    
+        <h2 className="text-[.9vw] font-medium text-center mb-[1vw]">Welcome again! </h2>
+        <TextField style={{width: '360px', height: '50px'}}  label="Email" variant="outlined" size="small" margin="normal" />
+        <TextField style={{width: '360px', height: '50px'}}  label="password" variant="outlined" size="small" margin="normal" />
+        <Button variant="contained" 
+          margin="normal"
+          size="large"
+          style={{
+              marginTop: '1vw',        
+          }}
+        >Login</Button>
+        <div className="flex items-center gap-4 py-[1vw]">
+          <div className="w-[6vw] h-[.1px] border-[.1px] border-zinc-300"></div>
+          <p>OR</p>
+          <div className="w-[6vw] h-[.1px] border-[.1px] border-zinc-300"></div>
+        </div>
+        <Link to="/signup" className="text-[.7vw]">Don&apos;t have an account <span className="underline text-blue-500 font-semibold">Signup</span></Link> 
+      </Card>
     </div>
   )
 }
