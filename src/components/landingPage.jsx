9b0ca401925598f7ddd798f305f8449cfce354fd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-17 15:08:32 +0530

    updated landing page

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import Appbar from "./Appbar"
+import axios from 'axios'
 
 
 const LandingPage = () => {
@@ -7,8 +8,19 @@ const LandingPage = () => {
   const [username, setUsername] = useState('');
 
   useEffect(() => {
+
+    const init() = async() => {
+      const res = await axios.get('http://localhost:3000/auth/me', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      })
+      let data = res.data;
+      setUsername(data.username)
+    }
     
-  })
+    init();
+  }, [])
 
   return (
     <div>
