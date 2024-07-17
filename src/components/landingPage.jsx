a258df86b7bf406e0ce7e821d6f1a9bfcd407fce
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-17 16:06:05 +0530

    updated landingpage

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -1,23 +1,13 @@
-import { useEffect, useState } from "react"
+
 import Appbar from "./Appbar"
-import axios from 'axios'
+
 
 
 const LandingPage = () => {
 
   
 
-  useEffect(() => {
-
-    const res = axios.get('http://localhost:3000/auth/me', {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`
-      }
-    })
-    const data = res.data;
-    console.log(data)
-  }, [])
-
+  
   
 
   return (
