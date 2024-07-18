c2ca393f000a77d1f490be73519af913067d1eab
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-18 21:35:09 +0530

    added

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -27,7 +27,7 @@ router.get('/todos', authenticateJwt, async(req, res) => {
   .then((todos) => {
     res.json(todos)
   })
-    .catch((err) => {
+  .catch((err) => {
         res.status(500).json({error: 'Failed to fetch todos'})
     })
 });
