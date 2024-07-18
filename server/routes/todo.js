791b4c51e6762423e426fd8df1ae7d04020603d9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-18 21:36:54 +0530

    added

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -36,7 +36,7 @@ router.patch('/todos/:todoId/done', authenticateJwt, (req, res) => {
   const { todoId } = req.params;
   const userId = req.userId;
   
-    Todo.findOneAndUpdate({ _id: todoId, userId }, { done: true }, { new: true })
+  Todo.findOneAndUpdate({ _id: todoId, userId }, { done: true }, { new: true })
       .then((updatedTodo) => {
         if (!updatedTodo) {
           return res.status(404).json({ error: 'Todo not found' });
