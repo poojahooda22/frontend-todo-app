ffd846aa50259a8b78030827427e9e25bcd7bdec
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-18 12:09:53 +0530

    updated todolist

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,34 @@
-
+import { Avatar, Button, Card, TextField } from "@mui/material"
 
 const TodoList = () => {
   return (
     <div>
-      Todo
+        <div className="px-[2vw] py-[2vw] flex items-center justify-between">
+            <h1 className="text-[1vw] font-semibold">Todo App</h1>
+            <div className="flex items-center gap-[1vw]">
+                <Button variant="contained">Logout</Button>
+                <Avatar src="/broken-image.jpg" />    
+            </div>
+        </div>
+        <div className="flex items-center justify-center">
+            <Card style={{
+                padding: '16px',   width: 460, 
+                display:"flex", flexDirection: "column", 
+                justifyContent: "center",  alignItems: "center",}}
+            >
+                <TextField style={{width: '360px', height: '50px'}}  
+                    label="Title" variant="outlined" size="small" margin="normal"
+                />
+                <TextField style={{width: '360px', height: '50px'}}  
+                    label="description" variant="outlined" size="small" margin="normal" 
+                />
+                <Button variant="contained" 
+                    margin="normal"
+                    size="large"
+                    style={{ marginTop: '1vw', }}
+                >Add</Button>
+            </Card>
+        </div>
     </div>
   )
 }
