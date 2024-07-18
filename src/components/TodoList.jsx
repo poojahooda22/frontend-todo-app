b2b03fc2c23c882d1161e476c3402c5f44e01923
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-18 15:32:41 +0530

    updated todo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -20,7 +20,7 @@ const TodoList = () => {
             setTodos(data);
         }
         getTodos();
-    })
+    }, [])
 
     const addTodo = async () => {
         const response = await fetch('http://localhost:3000/todo/todos', {
