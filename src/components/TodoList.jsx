6d436f37ff6b59d0d92682b9a8eca0d81c6bce71
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-07-20 21:34:33 +0530

    changes done text

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -114,7 +114,7 @@ const TodoList = () => {
                             <div className="flex flex-col gap-2">
                                 <button
                                     onClick={() => markDone(todo._id)}
-                                    className={`${todo.done ? 'bg-green-500' : 'border-[1px] border-green-500'} text-zinc-800 px-[.5vw] py-[.2vw] rounded-lg`}
+                                    className={`${todo.done ? 'bg-green-500 text-white' : 'border-[1px] border-green-500 text-zinc-800'}  px-[.5vw] py-[.2vw] rounded-lg`}
                                 >{todo.done ? 'Done' : 'Mark as done'}</button>
                                 <button
                                     className=""
